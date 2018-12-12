5c52d96d0074ad8a7cd604290a1d43b2c044e5fb
Lylian Person-Gay <lylian.person-gay@outlook.fr>
2018-12-12 15:48:00 +0100

    Ajout de plusieurs erreurs dans l'exercice 1-1.c

--- Exercice/exercice1-1.c
+++ Exercice/exercice1-1.c
@@ -1,11 +1,9 @@
 /* Exécutez le programme "bonjour, maître" sur votre ordinateur, en supprimant quelques parties du programme pour observer les erreurs." */
 
-#include <stdio.
+#include <stdio.h>
 
 main()
-{
    printf("bonjour, maître\n");
-}
 
 /* ERREURS :
 
@@ -16,3 +14,16 @@ exercice1-1.c:3:10: fatal error: stdio.: Aucun fichier ou dossier de ce type
  #include <stdio.
           ^
 compilation terminated.
+exercice1-1.c:5:1: warning: return type defaults to ‘int’ [-Wimplicit-int]
+ main()
+ ^~~~
+exercice1-1.c: In function ‘main’:
+exercice1-1.c:6:4: error: expected declaration specifiers before ‘printf’
+    printf("bonjour, maître\n");
+    ^~~~~~
+exercice1-1.c:8:1: error: unterminated comment
+ /* ERREURS :
+ ^
+exercice1-1.c:6:4: error: expected ‘{’ at end of input
+    printf("bonjour, maître\n");
+    ^~~~~~
