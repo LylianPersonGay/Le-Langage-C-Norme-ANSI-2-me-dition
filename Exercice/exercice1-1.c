a325c798f474543c1b20782fc0ca0a35aa4fc720
Lylian Person-Gay <lylian.person-gay@outlook.fr>
2018-12-12 15:44:23 +0100

    Ajout du dossier "Exercices" et du premier "Exercice1-1.c"

---
+++ Exercice/exercice1-1.c
@@ -0,0 +1,18 @@
+/* Exécutez le programme "bonjour, maître" sur votre ordinateur, en supprimant quelques parties du programme pour observer les erreurs." */
+
+#include <stdio.
+
+main()
+{
+   printf("bonjour, maître\n");
+}
+
+/* ERREURS :
+
+exercice1-1.c:3:17: error: missing terminating > character
+ #include <stdio.
+                 ^
+exercice1-1.c:3:10: fatal error: stdio.: Aucun fichier ou dossier de ce type
+ #include <stdio.
+          ^
+compilation terminated.
